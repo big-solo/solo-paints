82606903127735218c126d61ce103cf108318acc
solomon samuel <sammiejay111@gmail.com>
2026-09-20 14:45:55 +0100

    add image URL to contact model

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -26,7 +26,10 @@ const contactSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-
+    imageUrl: {
+        type: String,
+        default: ""
+    },
     createdAt: {
         type: Date,
         default: Date.now
